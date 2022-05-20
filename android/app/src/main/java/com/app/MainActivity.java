7fb168da97530ad69254872f62a13ae4a8a1097e
Guilherme <guilherme.fonseca@grana.capital>
2022-05-20 10:59:19 -0300

    changing yarn version and settings

--- android/app/src/main/java/com/app/MainActivity.java
+++ android/app/src/main/java/com/app/MainActivity.java
@@ -23,7 +23,6 @@ public class MainActivity extends ReactActivity {
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
     return new ReactActivityDelegate(this, getMainComponentName()) {
-      @Override
       protected ReactRootView createRootView() {
        return new RNGestureHandlerEnabledRootView(MainActivity.this);
       }
